34e06cc8436e983f85c1c64a74a78ce42b81c64e
TeeThana <127654807+TeeThana@users.noreply.github.com>
2024-03-26 22:20:50 +0700

    fix ui

--- src/screens/Friends/FriendsScreen.jsx
+++ src/screens/Friends/FriendsScreen.jsx
@@ -1,7 +1,16 @@
-import React ,{useEffect} from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'; // Import your desired icon library
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import tw from "twrnc";
+
+//Icons
+import { FontAwesome } from "@expo/vector-icons";
 
 //db
 import { db } from "../../../firebaseConfig";
@@ -15,183 +24,147 @@ import {
 } from "firebase/firestore";
 
 const FriendsScreen = () => {
-    const [text, setText] = React.useState("");
-    // const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const [user, setUser] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
 
-    const handleChangeText = (inputText) => {
-        setText(inputText);
-    };
+  const handleChangeText = (inputText) => {
+    setText(inputText);
+    searchUser(inputText);
+  };
 
-    useEffect(() => {
-        getUser();
-      }, []);
-    
-      const getUser = async () => {
-        const username = await AsyncStorage.getItem("username");
-        const docRef = collection(db,'UserInfo');
-        const docSnap = await getDocs(docRef);
-        docSnap.forEach((doc) => {
-            if (doc.exists()) {
-              const userData = doc.id;
-              console.log('User data:', userData);
-            } else {
-              console.log('User not found');
-            }
-          });
+  const getUser = async () => {
+    const username = await AsyncStorage.getItem("username");
+    const docRef = collection(db, "UserInfo");
+    const querySnapshot = await getDocs(docRef);
+    const userData = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.exists()) {
+        userData.push(doc.id);
+      }
+    });
+    setUser(userData);
+  };
 
-       
-      };
+  const searchUser = (searchText) => {
+    const filteredUsers = user.filter((userName) => {
+      return userName.includes(searchText);
+    });
+    setSearchResult(filteredUsers);
+  };
 
+  useEffect(() => {
+    getUser();
+  }, []);
 
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.greenContainer}>
-                <Text style={styles.textHeader}>Friends</Text>
-                <View style={{ height: 50 }}></View>
-                <View style={styles.inputContainer}>
-                    <Icon name="search" size={20} color="white" style={styles.icon} />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChangeText}
-                        value={text}
-                        placeholderTextColor="white"
-                        backgroundColor='#313047'
-                        placeholder="Search your friend or #"
-                    />
-                </View>
-            </View>
-
-            <View style={styles.whiteContainer}>
-                <View style={styles.height}></View>
-                <Text style={styles.textTitle}>Request friends</Text>
-                <View style={styles.height20}></View>
-                <Text style={styles.textSubTitle}>No request</Text>
-                <View style={styles.height20}></View>
-                <View style={{ flexDirection: 'row', }}
-                >
-                    <Text style={styles.textTitle}>Your friends </Text>
-                    <View style={{
-                        marginTop: 5,
-                        width: 20,
-                        height: 20,
-                        borderRadius: 50,
-                        backgroundColor: '#313047',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                        <Text style={{
-                            textAlign: 'center',
-                            color: 'white',
-                            fontSize: 16,
-                            fontWeight: 'bold',
-                        }}>3</Text>
-                    </View>
-                </View>
-
-                <View style={styles.height20}></View>
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-around'
-                }}>
-                    <View style={{ flexDirection: 'column', }}>
-                        <View style={styles.circle}>
-                        </View>
-                        <Text style={{ textAlign: 'center', }}>Gun</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'column', }}>
-                        <View style={styles.circle}>
-                        </View>
-                        <Text style={{ textAlign: 'center', }}>Tee</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'column', }}>
-                        <View style={styles.circle}>
-                        </View>
-                        <Text style={{ textAlign: 'center', }}>Fluke</Text>
-                    </View></View>
-
-
-            </View>
+  return (
+    <View style={tw`flex-1 bg-[#00D49D]`}>
+      <View style={tw`h-1/4`}>
+        <View
+          style={{ marginTop: "15%", ...tw`flex justify-center items-center` }}
+        >
+          <Text
+            style={{
+              fontFamily: "inter-bold",
+              ...tw`text-[#313047] text-2xl`,
+            }}
+          >
+            Friends
+          </Text>
+          <View
+            style={tw`mt-10 flex-row items-center bg-[#313047] rounded-xl px-5 w-4/5`}
+          >
+            <FontAwesome
+              name="search"
+              size={20}
+              color="white"
+              style={tw`mr-5`}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChangeText}
+              value={text}
+              placeholderTextColor="white"
+              backgroundColor="#313047"
+              placeholder="Search your friend or #"
+            />
+          </View>
         </View>
-    )
-}
+      </View>
+      <View style={{ ...tw`flex-1 h-full bg-[#F3EDF5]` }}></View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    whiteContainer: {
-        paddingLeft: 30,
-        paddingRight: 30,
-        flex: 3,
-        backgroundColor: 'white',
-        width: '100%',
-    },
-    greenContainer: {
-        height: 200,
-        backgroundColor: '#00D49D',
-        alignItems: 'center',
-        width: '100%',
-    },
-    textHeader: {
-        paddingTop: 30,
-        color: '#313047',
-        fontSize: 25,
-        fontWeight: 'bold',
-
-    },
-    textTitle: {
-        color: '#313047',
-        fontSize: 20,
-        fontWeight: 'bold',
-
-    },
-    textSubTitle: {
-        textAlign: 'center',
-        color: '#8F8F8F',
-        fontSize: 16,
-        fontWeight: 'regular',
-
-    },
-    height: {
-        height: 10
-    },
-    height20: {
-        height: 20
-    },
-    circle: {
-        width: 90,
-        height: 90,
-        borderWidth: 5,
-        borderRadius: 50,
-        borderColor: '#313047',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        width: '70%',
-        height: 40,
-        borderWidth: 0,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        fontSize: 18
-    },
-    icon: {
-        marginRight: 10,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#313047',
-        borderRadius: 15,
-        paddingHorizontal: 10,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  whiteContainer: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    flex: 3,
+    backgroundColor: "white",
+    width: "100%",
+  },
+  greenContainer: {
+    height: 200,
+    backgroundColor: "#00D49D",
+    alignItems: "center",
+    width: "100%",
+  },
+  textHeader: {
+    paddingTop: 30,
+    color: "#313047",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  textTitle: {
+    color: "#313047",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  textSubTitle: {
+    textAlign: "center",
+    color: "#8F8F8F",
+    fontSize: 16,
+    fontWeight: "regular",
+  },
+  height: {
+    height: 10,
+  },
+  height20: {
+    height: 20,
+  },
+  circle: {
+    width: 90,
+    height: 90,
+    borderWidth: 5,
+    borderRadius: 50,
+    borderColor: "#313047",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "70%",
+    height: 40,
+    borderWidth: 0,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontSize: 18,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#313047",
+    borderRadius: 15,
+    paddingHorizontal: 10,
+  },
 });
 
-
 export default FriendsScreen;
-
